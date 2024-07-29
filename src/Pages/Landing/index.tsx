132bc116d0e1402c2d7fcd86078fd132ad27962c
Gradac <marcosgradac2022@gmail.com>
2024-07-29 16:32:17 -0300

    add menu-top

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -1,24 +1,31 @@
-import { Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import './LandingPage.scss';
 export default function LandingPage() {
 	return (
 		<Grid className="landing-page" container>
 			<Grid className="landing-page-menu-top" container>
-				<Grid className='landing-page-logo' item xs={8}>
-					<Typography>CreacTivo + Logo</Typography>
+				<Grid className="landing-page-logo" item xs={8}>
+					<img src="/images/logo-creactivo.png" width="200px" />
 				</Grid>
-				<Grid item xs={1}>
-					<button>Inicio</button>
+				<Grid item xs={4}>
+					<Grid container>
+						<Grid className="button-menu-top" item xs={2.5}>
+							<Button variant="text">Inicio</Button>
+						</Grid>
+						<Grid className="button-menu-top" item xs={2.5}>
+							<Button variant="text">Curso</Button>
+						</Grid>
+						<Grid className="button-menu-top" item xs={2.5}>
+							<Button variant="text">Planes</Button>
+						</Grid>
+						<Grid className="button-menu-top" item xs={2.5}>
+							<Button variant="text">Contacto</Button>
+						</Grid>
+					</Grid>
 				</Grid>
-				<Grid item xs={1}>
-					<button>Inicio</button>
-				</Grid>
-				<Grid item xs={1}>
-					<button>Inicio</button>
-				</Grid>
-				<Grid item xs={1}>
-					<button>Inicio</button>
-				</Grid>
+			</Grid>
+			<Grid className="learn-landing" container>
+				<Typography>assadas</Typography>
 			</Grid>
 		</Grid>
 	);
